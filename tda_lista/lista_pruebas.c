7423a3bb70f90956191edabdc612bb4c6ec033ce
JuliTaras <julibtaras@gmail.com>
2020-05-29 23:13:52 -0300

    Se agregan las pruebas que faltan y el readme

--- tda_lista/lista_pruebas.c
+++ tda_lista/lista_pruebas.c
@@ -122,6 +122,32 @@ void pruebas_lista_apilar(){
     lista_destruir(lista);
 }
 
+void pruebas_lista_tope(){
+    printf("\nINICIO DE PRUEBAS LISTA TOPE\n");
+
+    /* Declaro las variables a utilizar*/
+    lista_t* lista = lista_crear();
+    int a = 1, b = 2, c = 3;
+
+    /* Inicio de pruebas */
+    prueba("Se inserta un elemento a la lista", lista_apilar(lista, &a) == 0);
+    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 1);
+    prueba("El tope de la pila es el elemento que apile", lista_tope(lista) == &a);
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+
+    prueba("Se inserta un elemento a la lista", lista_apilar(lista, &b) == 0);
+    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 2);
+    prueba("El tope de la pila es el elemento que apile", lista_tope(lista) == &b);
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+
+    prueba("Se inserta un elemento a la lista", lista_apilar(lista, &c) == 0);
+    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 3);
+    prueba("El tope de la pila es el elemento que apile", lista_tope(lista) == &c);
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+
+    lista_destruir(lista);
+}
+
 void pruebas_lista_desapilar(){
     printf("\nINICIO DE PRUEBAS LISTA DESAPILAR\n");
 
@@ -265,7 +291,6 @@ void pruebas_lista_borrar_de_posicion(){
     lista_destruir(lista);
 }
 
-
 void pruebas_lista_encolar(){
     printf("\nINICIO DE PRUEBAS LISTA ENCOLAR\n");
 
@@ -294,6 +319,24 @@ void pruebas_lista_encolar(){
     lista_destruir(lista);
 }
 
+void pruebas_lista_primero(){
+    printf("\nINICIO DE PRUEBAS LISTA PRIMERO\n");
+
+    /* Declaro las variables a utilizar*/
+    lista_t* lista = lista_crear();
+    int a = 1, b = 2, c = 3;
+
+    /* Inicio de pruebas */
+    prueba("Se encola un elemento a la lista", lista_encolar(lista, &a) == 0);
+    prueba("Primer elemento de la lista", lista_primero(lista) == &a);
+    prueba("Se encola un elemento a la lista", lista_encolar(lista, &b) == 0);
+     prueba("Primer elemento de la lista", lista_primero(lista) == &b);
+    prueba("Se encola un elemento a la lista", lista_encolar(lista, &c) == 0);
+     prueba("Primer elemento de la lista", lista_primero(lista) == &c);
+
+    lista_destruir(lista);
+}
+
 void pruebas_lista_desencolar(){
     printf("\nINICIO DE PRUEBAS LISTA DESENCOLAR\n");
 
@@ -309,25 +352,73 @@ void pruebas_lista_desencolar(){
 
     prueba("Se encola un elemento a la lista", lista_encolar(lista, &b) == 0);
     prueba("La lista tiene un elemento mas", lista_elementos(lista) == 2);
-    prueba("Elemento en posicion", lista_elemento_en_posicion(lista, 0) == &b);
+    prueba("Elemento en posicion 0", lista_elemento_en_posicion(lista, 0) == &b);
     prueba("La lista no esta vacia", !lista_vacia(lista));
 
     prueba("Se encola un elemento a la lista", lista_encolar(lista, &c) == 0);
     prueba("La lista tiene un elemento mas", lista_elementos(lista) == 3);
-    prueba("Elemento en posicion", lista_elemento_en_posicion(lista, 0) == &c);
+    prueba("Elemento en posicion 0", lista_elemento_en_posicion(lista, 0) == &c);
     prueba("La lista no esta vacia", !lista_vacia(lista));
 
     prueba("Se encola un elemento a la lista", lista_encolar(lista, &d) == 0);
     prueba("La lista tiene un elemento mas", lista_elementos(lista) == 4);
-    prueba("Elemento en posicion", lista_elemento_en_posicion(lista, 0) == &d);
+    prueba("Elemento en posicion 0", lista_elemento_en_posicion(lista, 0) == &d);
     prueba("La lista no esta vacia", !lista_vacia(lista));
 
     prueba("Se desenencola un elemento a la lista", lista_desencolar(lista) == 0);
-    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 3);
+    prueba("La lista tiene un elemento menos", lista_elementos(lista) == 3);
+
+    prueba("Se desenencola un elemento a la lista", lista_desencolar(lista) == 0);
+    prueba("La lista tiene un elemento menos", lista_elementos(lista) == 2);
+
+    prueba("Se desenencola un elemento a la lista", lista_desencolar(lista) == 0);
+    prueba("La lista tiene un elemento menos", lista_elementos(lista) == 1);
+
+    prueba("Se desenencola un elemento a la lista", lista_desencolar(lista) == 0);
+    prueba("La lista esta vacia", lista_vacia(lista));
+
     lista_destruir(lista);
 }
 
-/*Revisar que prubas faltan*/
+void pruebas_lista_ultimo(){
+    printf("\nINICIO DE PRUEBAS LISTA ULTIMO\n");
+
+    /* Declaro las variables a utilizar*/
+    lista_t* lista = lista_crear();
+    int a = 1, b = 2;
+
+    /* Inicio de pruebas */
+    prueba("Se inserta un elemento a la lista", lista_insertar(lista, &a) == 0);
+    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 1);
+    prueba("El ultimo elemento de la lista es el que inserte", lista_ultimo(lista) == &a);
+    prueba("Elemento en posicion 0", lista_elemento_en_posicion(lista, 0) == &a);
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+
+    prueba("Se inserta un elemento a la lista", lista_insertar(lista, &b) == 0);
+    prueba("La lista tiene un elemento mas", lista_elementos(lista) == 2);
+    prueba("Elemento en posicion 1", lista_elemento_en_posicion(lista, 1) == &b);
+    prueba("El ultimo elemento de la lista es el que inserte", lista_ultimo(lista) == &b);
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+
+    lista_destruir(lista);
+}
+
+void pruebas_lista_volumen() {
+    printf("\nINICIO DE PRUEBAS LISTA VOLUMEN\n");
+
+    /* Declaro las variables a utilizar*/
+    lista_t* lista = lista_crear();
+
+    printf("Se insertaran 25 elementos\n");
+    for (size_t i = 0; i < 25; i++){
+        prueba("Se inserta un elemento a la lista", lista_insertar(lista, &i) == 0);
+    }
+
+    prueba("La lista no esta vacia", !lista_vacia(lista));
+    prueba("La lista tiene 25 elementos", lista_elementos(lista) == 25);
+
+    lista_destruir(lista);
+}
 
 int main(){
    pruebas_lista_crear();
@@ -342,5 +433,9 @@ int main(){
    pruebas_lista_borrar_de_posicion();
    pruebas_lista_encolar();
    pruebas_lista_desencolar();
+   pruebas_lista_ultimo();
+   pruebas_lista_tope();
+   pruebas_lista_primero();
+   pruebas_lista_volumen();
    return 0;
 }
